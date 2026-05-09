6a149c727b40a02624f71b82b74c9e012ebc6c56
HsrRaza <hr.raza04@gmail.com>
2026-05-09 11:11:45 +0530

    feat added a node course progress

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import ReactPage from './pages/ReactPage'
 import HomePage from './pages/HomePage'
 import JavaScriptPage from './pages/JavaScriptPage'
+import NodePage from './pages/NodePage'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <Route path='/' element= {<HomePage/>} />
         <Route path="/react" element={<ReactPage/>}/>
         <Route path="/javascript" element={<JavaScriptPage/>}/>
+        <Route path="/node" element={<NodePage/>}/>
       </Routes>
    
   )
