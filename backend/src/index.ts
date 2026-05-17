c3d249a4c919f4dbe0c6ae145cd3eddec92c8cb4
HsrRaza <hr.raza04@gmail.com>
2026-05-18 00:53:36 +0530

    db setup

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import {pool} from "./config/db.js";
+import dotenv from "dotenv";
+import  type { Response , Request } from "express";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req:Request, res:Response) => {
+  const result = await pool.query("SELECT NOW()");
+
+  res.json({
+    message: "Backend connected to PostgreSQL Docker",
+    time: result.rows[0],
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
